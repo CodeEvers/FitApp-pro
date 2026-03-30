31535f356994a67c37179f08472d20cc16ad13a5
CodeEvers <g2agimly@gmail.com>
2026-03-30 07:18:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     auth: {
-        persistSession: false,
-        autoRefreshToken: false
+        persistSession: true,
+        autoRefreshToken: true
     }
 });
 
